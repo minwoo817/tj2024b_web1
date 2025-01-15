93e870996fc22d4213a8ab8e269630a1f979ef0a
gitminwoo817 <gitcapzhang123@naver.com>
2025-01-15 15:29:10 +0900

    .

---
+++ tj2024b_web1/src/main/java/day01/Task3.java
@@ -0,0 +1,15 @@
+package day01;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Task3 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Task3 실행");
+	}
+}
